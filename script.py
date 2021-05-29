4791e771580325c4e64d4c14cce494ab381a40c0
JINN-OF-TG <84127769+JINN-OF-TG@users.noreply.github.com>
2021-05-29 16:59:56 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,60 +1,24 @@
 class Script(object):
 
     START_MSG = """ഹായ്👋 {},
-    ഞാൻ ഒരു ഗ്രൂപ്പ് ഫിൽട്ടർ ബോട്ട് ആണ്, എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാൻ നോക്കി സമയം കളയണ്ട, എന്നെ CARNIVAL CINEMAS ഗ്രൂപ്പിൽ മാത്രമേ ആഡ് ചെയ്യാൻ കഴിയൂ...!!!
+    ഞാൻ ഒരു ഗ്രൂപ്പ് ഫിൽട്ടർ ബോട്ട് ആണ്, എൻ്റെ വിധി എൻ്റെ [അഡ്മിൻ്റ](https://t.me/Rahelvakkachan) തീരുമാനങ്ങളാണ്.എന്നെ CARNIVAL CINEMAS ഗ്രൂപ്പിൽ മാത്രമേ ആഡ് ചെയ്യാൻ കഴിയൂ...!!!
 
 """
 
 
     HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
+എനിക്ക് നിൻ്റെ സഹായം ഒന്നും വേണ്ട😎😎. 
 
 
-<b>Basic Commands;</b>
-
-/start - Check if I'm alive!
-/help - Command help
-/about - Something about me!
 
 
-<b>Filter Commands;</b>
 
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
-<code>/viewfilters</code>  -  List all filters in chat
-
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
-<code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-
-/id  -  Shows ID information
-
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
-<b>© @TroJanzHEX</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """ ഞാൻ ഒരു മഹാ സംഭവമാണ്😜🤔😎. 
 
-⭕️<b>Creater :</b> @TroJanzHEX    
 
-⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
 """
